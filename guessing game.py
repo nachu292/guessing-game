be815ce5fb4556025821943e5f34696b9d0a67bf
ANOOP KV <anuanoopkv@gmail.com>
2025-07-17 17:22:15 +0530

    Add files via upload

---
+++ guessing game.py
@@ -0,0 +1,19 @@
+import os
+
+# Keep asking until a valid single-digit number is entered
+while True:
+    real = input("enter a one-digit number: ")
+    if not (real.isdigit() and len(real) == 1):
+        print("please enter a valid one-digit number")
+    else:
+        os.system('cls')  # Clear the console on Windows
+        break  # Exit the loop when a valid number is entered
+
+# Guessing loop
+while True:
+    number = input("guess the number: ")
+    if number == real:
+        print("you won")
+        break
+    else:
+        print("Incorrect guess. Please try again!")
